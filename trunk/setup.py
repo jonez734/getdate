4f69bba1771e8eaf158162d472eac4ae1e140f8d
Jeff MacDonald <jam@zoidtechnologies.com>
2020-12-10 19:37:00 -0500

    getdate/trunk/setup.py: added 'classifiers' and 'ext_modules'; added some code to check if getdate-parser.y is newer than 'getdate-parser.c', spawn 'make'.

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,11 +2,19 @@
 
 import time
 from distutils.core import setup, Extension
+from distutils.util import newer, spawn
 
 v = time.strftime("%Y%m%d%H%M")
 projectname = "getdate"
 
 #module1 = Extension("libgetdate") #, sources=["getdate.tab.c", "getdate.c"])
+
+extmodules = []
+extmodules.append(Extension("libgetdate", ["pygetdate.c", "getdate-parser.c", "getdate-lexer-original.c", "getdate-timezones.c"]))
+grammary = "getdate-parser.y"
+grammarc = "getdate-parser.c"
+if newer(grammary, grammarc):
+  spawn(["make", grammarc], verbose=1)
 
 setup(
   name=projectname,
@@ -15,5 +23,14 @@ setup(
   author="zoid technologies",
   author_email="%s@projects.zoidtechnologies.com" % (projectname),
   py_modules=["getdate",],
-  ext_modules=[Extension("libgetdate", ["pygetdate.c", "getdate-parser.c", "getdate-lexer-original.c", "getdate-timezones.c"])] # module1,]
+#  headers=["xtime.h"],
+#  ext_modules=[Extension("libgetdate", ["pygetdate.c", "getdate-parser.c", "getdate-lexer-original.c", "getdate-timezones.c"])] # module1,]
+  ext_modules=extmodules,
+  classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development :: Libraries",
+  ]
 )
